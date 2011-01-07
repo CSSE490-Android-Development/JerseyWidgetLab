92e5975570ef459aa10fa4d4554264d1cd6799df
Eric Stokes <fernferret@gmail.com>
2011-01-07 02:33:24 -0500

    Add EditJersey Activity

---
+++ CustomJersey/src/com/fernferret/customjersey/EditJersey.java
@@ -0,0 +1,12 @@
+package com.fernferret.customjersey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditJersey extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit);
+	}
+}
