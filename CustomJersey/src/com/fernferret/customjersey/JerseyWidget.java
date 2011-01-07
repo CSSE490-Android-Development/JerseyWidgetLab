70ade24eb15f23eea99363f435f8b4f340fcc161
Eric Stokes <fernferret@gmail.com>
2011-01-07 14:24:01 -0500

    Add initial widget

---
+++ CustomJersey/src/com/fernferret/customjersey/JerseyWidget.java
@@ -0,0 +1,12 @@
+package com.fernferret.customjersey;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class JerseyWidget extends AppWidgetProvider {
+
+}
