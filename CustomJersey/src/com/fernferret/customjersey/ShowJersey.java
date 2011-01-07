96bec0311146956a6ad2b2cf45b98a302e373094
Eric Stokes <fernferret@gmail.com>
2011-01-07 02:54:11 -0500

    Make ShowJersey launch EditJersey

--- CustomJersey/src/com/fernferret/customjersey/ShowJersey.java
+++ CustomJersey/src/com/fernferret/customjersey/ShowJersey.java
@@ -1,13 +1,35 @@
 package com.fernferret.customjersey;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ShowJersey extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	static final String PLAYER_NAME = "PLAYERNAME";
+	static final String PLAYER_NUMBER = "PLAYERNUMBER";
+	static final int EDIT_JERSEY_REQUEST_CODE = 0;
+	
+	private Button mEditButton;
+	private String mPlayerName;
+	private String mPlayerNumber;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mEditButton = (Button) findViewById(R.id.edit_button);
+		mEditButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent editIntent = new Intent(ShowJersey.this, EditJersey.class);
+				editIntent.putExtra(PLAYER_NAME, mPlayerName);
+				editIntent.putExtra(PLAYER_NUMBER, mPlayerNumber);
+				startActivityForResult(editIntent, EDIT_JERSEY_REQUEST_CODE);
+			}
+		});
+	}
 }
